15de5eb91d6c04881b707d697247870908c30638
marisolGarro <141685137+marisolGarro@users.noreply.github.com>
2023-10-03 19:06:47 -0300

    base de dato

---
+++ Base de dato/bomberos.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 03-10-2023 a las 00:33:21
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bomberos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `bombero`
+--
+
+CREATE TABLE `bombero` (
+  `idBombero` int(11) NOT NULL,
+  `dni` varchar(8) NOT NULL,
+  `nombreApellido` varchar(50) NOT NULL,
+  `fechaNac` date NOT NULL,
+  `celular` varchar(15) NOT NULL,
+  `codBrigada` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `brigada`
+--
+
+CREATE TABLE `brigada` (
+  `codBrigada` int(11) NOT NULL,
+  `nombreBriga` varchar(20) NOT NULL,
+  `especialidad` varchar(30) NOT NULL,
+  `libre` tinyint(1) NOT NULL,
+  `nroCuartel` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cuartel`
+--
+
+CREATE TABLE `cuartel` (
+  `codCuartel` int(11) NOT NULL,
+  `nombreCuartel` varchar(20) NOT NULL,
+  `direccion` varchar(30) NOT NULL,
+  `coord_X` int(11) NOT NULL,
+  `coord_Y` int(11) NOT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `correo` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `siniestro`
+--
+
+CREATE TABLE `siniestro` (
+  `codigo` int(11) NOT NULL,
+  `tipo` varchar(20) NOT NULL,
+  `fechaSiniestro` date NOT NULL,
+  `coord_X` int(11) NOT NULL,
+  `coord_Y` int(11) NOT NULL,
+  `detalle` text NOT NULL,
+  `fechaResol` date NOT NULL,
+  `puntuacion` int(11) NOT NULL,
+  `codBrigada` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `bombero`
+--
+ALTER TABLE `bombero`
+  ADD PRIMARY KEY (`idBombero`),
+  ADD UNIQUE KEY `dni` (`dni`);
+
+--
+-- Indices de la tabla `brigada`
+--
+ALTER TABLE `brigada`
+  ADD PRIMARY KEY (`codBrigada`);
+
+--
+-- Indices de la tabla `cuartel`
+--
+ALTER TABLE `cuartel`
+  ADD PRIMARY KEY (`codCuartel`);
+
+--
+-- Indices de la tabla `siniestro`
+--
+ALTER TABLE `siniestro`
+  ADD PRIMARY KEY (`codigo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `bombero`
+--
+ALTER TABLE `bombero`
+  MODIFY `idBombero` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `brigada`
+--
+ALTER TABLE `brigada`
+  MODIFY `codBrigada` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cuartel`
+--
+ALTER TABLE `cuartel`
+  MODIFY `codCuartel` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `siniestro`
+--
+ALTER TABLE `siniestro`
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
