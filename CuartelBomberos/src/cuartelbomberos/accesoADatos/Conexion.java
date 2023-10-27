c2f2fea20c3442d805504e4cc1408b77e0553d5b
NoSheepForDrake <albaty222@gmail.com>
2023-10-27 18:40:30 -0300

    ultimo cambio

--- CuartelBomberos/src/cuartelbomberos/accesoADatos/Conexion.java
+++ CuartelBomberos/src/cuartelbomberos/accesoADatos/Conexion.java
@@ -10,7 +10,7 @@ import javax.swing.JOptionPane;
 public class Conexion {
 
     private static final String URL = "jdbc:mariadb://localhost/";
-    private static final String DB = "bomberos";
+    private static final String DB = "bomberosG37";
     private static final String USUARIO = "root";
     private static final String PASSWORD = "";
     private static Connection conexion;
